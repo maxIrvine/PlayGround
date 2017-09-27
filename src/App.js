cbe348575cdc09a914daa3f0368466e9cce5181e
Max Irvine <max.irvine99@gmail.com>
2017-09-27 12:54:12 -0400

    adds basic hello

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
